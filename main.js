a4396d2701be538fda2596c643029e65723a0850
Jesus Gomez <2708jesusgz@gmail.com>
2025-05-26 08:35:53 -0600

    Correccion de Ortografia2

--- main.js
+++ main.js
@@ -21,13 +21,13 @@ document.addEventListener("DOMContentLoaded", () => {
       hola.textContent = `Y podría seguir así muchísimo… Sabes que me encanta enlistarte lo que me gusta de ti cada que puedo.`;
     } else if (clickCount === 5) {
       hola.textContent = `Me siento el más afortunado y agradecido por tenerte en mi vida y porque me permitas ser parte de la tuya.`;
-    } else if (clickCount === 5) {
-      hola.textContent = `He amado cada minuto, y jamás me he arrepentido de decidir amarte cada día de mi vida desde que te conozco.`;
     } else if (clickCount === 6) {
-      hola.textContent = `No te miento cuando te digo que estos últimos días me has hecho tanta falta. Te he extrañado muchísimo, no he soportado no verte.`;
+      hola.textContent = `He amado cada minuto, y jamás me he arrepentido de decidir amarte cada día de mi vida desde que te conozco.`;
     } else if (clickCount === 7) {
-      hola.textContent = `He estado a punto de llorar solo de pensar en cuánto te extraño y en las ganas inmensas que tengo de volver a verte.`;
+      hola.textContent = `No te miento cuando te digo que estos últimos días me has hecho tanta falta. Te he extrañado muchísimo, no he soportado no verte.`;
     } else if (clickCount === 8) {
+      hola.textContent = `He estado a punto de llorar solo de pensar en cuánto te extraño y en las ganas inmensas que tengo de volver a verte.`;
+    } else if (clickCount === 9) {
       hola.textContent = `Cuando no estoy contigo, realmente siento que me falta algo, como si no tuviera todo lo que necesito para vivir como me gustaría.`;
     } else if (clickCount === 10) {
       hola.textContent = `Te amo millones, espero poder seguir compartiendo mi vida contigo y que tu compartas la tuya conmigo, estoy muy feliz de haber conocido a la Sofia foranea que seguia en la uni y le gustaban las mismas canciones que a mi`;
@@ -35,9 +35,9 @@ document.addEventListener("DOMContentLoaded", () => {
       hola.textContent = `Soy el más feliz de haber conocido a la Sofía foránea que estaba cursando la uni y le gustaban las mismas canciones que a mí.`;
     } else if (clickCount === 11) {
       hola.textContent = `Y soy el más orgulloso de la Sofía en la que te has convertido: igual de auténtica, divertida, interesante, encantadora, guapa, una compañía increíble, excelente besadora y con una voz que podría escuchar todo el día`;
-    } else if (clickCount === 11) {
-      hola.textContent = `Solo quiero decirte que...`;
     } else if (clickCount === 12) {
+      hola.textContent = `Solo quiero decirte que...`;
+    } else if (clickCount === 13) {
       hola.classList.remove("text-justify");
       hola.classList.add("text-center");
       hola.textContent = `Te amo, gracias por todo`;
